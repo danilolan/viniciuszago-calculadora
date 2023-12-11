fa2d77f013450274f61c360cccbf8b8a54a8a2b7
danilolan <didoherculano@gmail.com>
2023-12-11 17:10:32 -0300

    feat: loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 "use client"
 
-import { ThemeProvider } from "@mui/material"
+import { CircularProgress, ThemeProvider } from "@mui/material"
 import { AppTheme } from "@/themes"
 import Calculator from "@/components/Calculator"
+import { useState } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  setTimeout(() => setIsLoading(false), 500)
+
   return (
     <ThemeProvider theme={AppTheme}>
       <main className="flex items-center justify-center text-primary w-full">
+        {
+          isLoading && 
+          <div className="z-10 w-screen h-screen bg-primary absolute flex items-center justify-center">
+            <CircularProgress color="secondary" />
+          </div>
+        }
         <Calculator/>
       </main>
     </ThemeProvider>
